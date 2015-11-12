b06d2526b42900bfa7cf8f6d04ba2be28a57b549
Paolo Cifariello <paolocifa@gmail.com>
2015-11-12 18:06:11 +0100

    Initial Commit

---
+++ Top-k-prefix/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Top-k-prefix
+//
+//  Created by Paolo Cifariello on 12/11/15.
+//  Copyright © 2015 Paolo Cifariello. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
